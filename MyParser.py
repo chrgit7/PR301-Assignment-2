895ffef5d678d3557dbda99d19df1463ebf8638d
Chr <36724772+chrgit7@users.noreply.github.com>
2019-09-21 02:03:59 +1200

    Adding a lookup table

--- MyParser.py
+++ MyParser.py
@@ -4,6 +4,20 @@ from TIGr import AbstractParser
 class MyParser(AbstractParser):
     def __init__(self, drawer):
         super().__init__(drawer)
+        self.draw_command = {
+            'D': lambda: self.drawer.pen_down(),
+            'P': lambda: self.drawer.select_pen(self.data),
+            'N': lambda: self.drawer.draw_line(0, self.data),
+            'E': lambda: self.drawer.draw_line(90, self.data),
+            'S': lambda: self.drawer.draw_line(180, self.data),
+            'W': lambda: self.drawer.draw_line(270, self.data),
+            'X': lambda: self.drawer.go_along(self.data),
+            'Y': lambda: self.drawer.go_down(self.data),
+            'U': lambda: self.drawer.pen_up(),
+            'C': lambda: self.drawer.draw_circle(self.data),
+            'R': lambda: self.drawer.draw_rectangle(self.data),
+            'T': lambda: self.drawer.draw_triangle(self.data)
+        }
 
     def parse(self, raw_source):
         self.source = raw_source
